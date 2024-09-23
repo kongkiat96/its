7417628ab58acd2311683bacb02865d8ddca87bc
kongkiat.b <kongkiat.b@srisawadpower.com>
2024-09-23 20:36:46 +0700

    DB UAT

--- core/config.core.php
+++ core/config.core.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'localhost:3306');
 
 // MySQL database username
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'systemit_uat_its');
 
 // MySQL database password
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'i8ut4X&60');
 
 // MySQL database name
-define('DB_NAME', 'its');
+define('DB_NAME', 'systemit_uat_its');
