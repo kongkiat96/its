74de1be54175ca763d7bcc58d46998d64a0a9895
kongkiat.b <kongkiat.b@srisawadpower.com>
2024-09-23 20:32:59 +0700

    Set DB & Patch IMG

---
+++ core/config.core.php
@@ -0,0 +1,12 @@
+<?php
+// MySQL hostname
+define('DB_HOST', 'localhost:3306');
+
+// MySQL database username
+define('DB_USERNAME', 'systemit_uat_its');
+
+// MySQL database password
+define('DB_PASSWORD', 'i8ut4X&60');
+
+// MySQL database name
+define('DB_NAME', 'systemit_uat_its');
